5cdfffd56e87b09c8bb4d2f5b8c6701368fa0f6a
Dhanasekar Subramaniam <dhanasekars@gmail.com>
2024-09-29 07:12:43 +0530

    get endpoint

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, HTTPException, Depends
+from sqlalchemy.future import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from db import get_db
+from models import Student
 
 app = FastAPI()
 
 @app.get("/v1/healthcheck")
 async def healthcheck():
-    return JSONResponse(content={"status": "up and running"}, status_code=200)
+    return {"status": "Healthy!"}
+
+@app.get("/v1/students")
+async def get_students(db: AsyncSession = Depends(get_db)):
+    try:
+        # Query the students table
+        result = await db.execute(select(Student))
+        students_list = result.scalars().all()
+
+        if not students_list:
+            raise HTTPException(status_code=404, detail="No students found")
+
+        # Return the list of students
+        return students_list
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error retrieving students: {str(e)}")
+
+
+
 
 if __name__ == "__main__":
     import uvicorn
---
+++ models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String, Date
+from db import Base
+
+class Student(Base):
+    __tablename__ = 'students'
+
+    id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String(100), index=True)
+    last_name = Column(String(100), index=True)
+    email = Column(String(100), unique=True, index=True)
+    date_of_birth = Column(Date)
